5ccb0f2cf45b684bb76eab252e1b8e13091f56f3
Shuai <liustark@hotmail.com>
2019-05-07 21:08:09 +0800

    prepare for train

--- YOLOv3-Keras/model.py
+++ YOLOv3-Keras/model.py
@@ -14,59 +14,59 @@ from keras.regularizers import l2
 from utils import compose
 
 
-# @wraps(Conv2D)
-# def DarknetConv2D(*args, **kwargs):
-#     """Wrapper to set Darknet parameters for Convolution2D."""
-#     darknet_conv_kwargs = {'kernel_regularizer': l2(5e-4)}
-#     darknet_conv_kwargs['padding'] = 'valid' if kwargs.get('strides') == (2, 2) else 'same'
-#     darknet_conv_kwargs.update(kwargs)
-#     return Conv2D(*args, **darknet_conv_kwargs)
-#
-#
-# def DarknetConv2D_BN_Leaky(*args, **kwargs):
-#     """Darknet Convolution2D followed by BatchNormalization and LeakyReLU."""
-#     no_bias_kwargs = {'use_bias': False}
-#     no_bias_kwargs.update(kwargs)
-#     return compose(
-#         DarknetConv2D(*args, **no_bias_kwargs),
-#         BatchNormalization(),
-#         LeakyReLU(alpha=0.1))
-#
-#
-# def tiny_yolo_body(inputs, num_anchors, num_classes):
-#     '''Create Tiny YOLO_v3 model CNN body in keras.'''
-#     x1 = compose(
-#         DarknetConv2D_BN_Leaky(16, (3, 3)),
-#         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
-#         DarknetConv2D_BN_Leaky(32, (3, 3)),
-#         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
-#         DarknetConv2D_BN_Leaky(64, (3, 3)),
-#         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
-#         DarknetConv2D_BN_Leaky(128, (3, 3)),
-#         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
-#         DarknetConv2D_BN_Leaky(256, (3, 3)))(inputs)
-#     x2 = compose(
-#         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
-#         DarknetConv2D_BN_Leaky(512, (3, 3)),
-#         MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='same'),
-#         DarknetConv2D_BN_Leaky(1024, (3, 3)),
-#         DarknetConv2D_BN_Leaky(256, (1, 1)))(x1)
-#     y1 = compose(
-#         DarknetConv2D_BN_Leaky(512, (3, 3)),
-#         DarknetConv2D(num_anchors * (num_classes + 5), (1, 1)))(x2)
-#     # 卷积层的最后，如何得出具有实际意义的最后的feature map，上面传入的参数很重要，
-#     # 最后输入keras的内置函数Conv2D(*args, **darknet_conv_kwargs)
-#     # 具体做了什么需要进一步探究
-#
-#     x2 = compose(
-#         DarknetConv2D_BN_Leaky(128, (1, 1)),
-#         UpSampling2D(2))(x2)
-#     y2 = compose(
-#         Concatenate(),
-#         DarknetConv2D_BN_Leaky(256, (3, 3)),
-#         DarknetConv2D(num_anchors * (num_classes + 5), (1, 1)))([x2, x1])
-#
-#     return Model(inputs, [y1, y2])
+@wraps(Conv2D)
+def DarknetConv2D(*args, **kwargs):
+    """Wrapper to set Darknet parameters for Convolution2D."""
+    darknet_conv_kwargs = {'kernel_regularizer': l2(5e-4)}
+    darknet_conv_kwargs['padding'] = 'valid' if kwargs.get('strides') == (2, 2) else 'same'
+    darknet_conv_kwargs.update(kwargs)
+    return Conv2D(*args, **darknet_conv_kwargs)
+
+
+def DarknetConv2D_BN_Leaky(*args, **kwargs):
+    """Darknet Convolution2D followed by BatchNormalization and LeakyReLU."""
+    no_bias_kwargs = {'use_bias': False}
+    no_bias_kwargs.update(kwargs)
+    return compose(
+        DarknetConv2D(*args, **no_bias_kwargs),
+        BatchNormalization(),
+        LeakyReLU(alpha=0.1))
+
+
+def tiny_yolo_body(inputs, num_anchors, num_classes):
+    '''Create Tiny YOLO_v3 model CNN body in keras.'''
+    x1 = compose(
+        DarknetConv2D_BN_Leaky(16, (3, 3)),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        DarknetConv2D_BN_Leaky(32, (3, 3)),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        DarknetConv2D_BN_Leaky(64, (3, 3)),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        DarknetConv2D_BN_Leaky(128, (3, 3)),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        DarknetConv2D_BN_Leaky(256, (3, 3)))(inputs)
+    x2 = compose(
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        DarknetConv2D_BN_Leaky(512, (3, 3)),
+        MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='same'),
+        DarknetConv2D_BN_Leaky(1024, (3, 3)),
+        DarknetConv2D_BN_Leaky(256, (1, 1)))(x1)
+    y1 = compose(
+        DarknetConv2D_BN_Leaky(512, (3, 3)),
+        DarknetConv2D(num_anchors * (num_classes + 5), (1, 1)))(x2)
+    # 卷积层的最后，如何得出具有实际意义的最后的feature map，上面传入的参数很重要，
+    # 最后输入keras的内置函数Conv2D(*args, **darknet_conv_kwargs)
+    # 具体做了什么需要进一步探究
+
+    x2 = compose(
+        DarknetConv2D_BN_Leaky(128, (1, 1)),
+        UpSampling2D(2))(x2)
+    y2 = compose(
+        Concatenate(),
+        DarknetConv2D_BN_Leaky(256, (3, 3)),
+        DarknetConv2D(num_anchors * (num_classes + 5), (1, 1)))([x2, x1])
+
+    return Model(inputs, [y1, y2])
 
 
 def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
@@ -82,6 +82,7 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     anchors_tensor = K.reshape(K.constant(anchors), [1, 1, 1, num_anchors, 2])
 
     grid_shape = K.shape(feats)[1:3]  # height, width
+
     grid_y = K.tile(K.reshape(K.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]),
                     [1, grid_shape[1], 1, 1])
     grid_x = K.tile(K.reshape(K.arange(0, stop=grid_shape[1]), [1, -1, 1, 1]),
@@ -97,7 +98,7 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     box_wh = K.exp(feats[..., 2:4]) * anchors_tensor / K.cast(input_shape[::-1], K.dtype(feats))
     box_confidence = K.sigmoid(feats[..., 4:5])
     box_class_probs = K.sigmoid(feats[..., 5:])
-    
+
     # grid包含grid的位置
     print(grid, feats, box_xy, box_wh)
     # Tensor("Cast_4:0", shape=(?, ?, 1, 2), dtype=float32) Tensor("Reshape_9:0", shape=(?, ?, ?, 3, 85), dtype=float32) Tensor("truediv_8:0", shape=(?, ?, ?, 3, 2), dtype=float32) Tensor("truediv_9:0", shape=(?, ?, ?, 3, 2), dtype=float32)
@@ -121,6 +122,7 @@ def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
     box_yx = (box_yx - offset) * scale
     box_hw *= scale
 
+    # box_mins, box_maxes. what's that mean?
     box_mins = box_yx - (box_hw / 2.)
     box_maxes = box_yx + (box_hw / 2.)
     boxes = K.concatenate([
@@ -137,8 +139,11 @@ def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
 
 def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape):
     '''Process Conv layer output'''
+
+    # Convert final layer features to bounding box parameters.
     box_xy, box_wh, box_confidence, box_class_probs = yolo_head(feats,
                                                                 anchors, num_classes, input_shape)
+    # Scale boxes back to original image shape.
     boxes = yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape)
     boxes = K.reshape(boxes, [-1, 4])
     box_scores = box_confidence * box_class_probs
@@ -163,13 +168,14 @@ def yolo_eval(yolo_outputs,
     boxes = []
     box_scores = []
     for l in range(num_layers):
-        _boxes, _box_scores = yolo_boxes_and_scores(yolo_outputs[l],
+        _boxes, _box_scores =  (yolo_outputs[l],
                                                     anchors[anchor_mask[l]], num_classes, input_shape, image_shape)
         boxes.append(_boxes)
         box_scores.append(_box_scores)
     boxes = K.concatenate(boxes, axis=0)
     box_scores = K.concatenate(box_scores, axis=0)
 
+    # 需要首先过滤掉置信度太小的，小于score_threshold的，这里设置好mask
     mask = box_scores >= score_threshold
     max_boxes_tensor = K.constant(max_boxes, dtype='int32')
     boxes_ = []
@@ -179,6 +185,8 @@ def yolo_eval(yolo_outputs,
         # TODO: use keras backend instead of tf.
         class_boxes = tf.boolean_mask(boxes, mask[:, c])
         class_box_scores = tf.boolean_mask(box_scores[:, c], mask[:, c])
+
+        # 使用的内置NMS函数！之前怀疑的NMS出错了，究竟是什么原因？
         nms_index = tf.image.non_max_suppression(
             class_boxes, class_box_scores, max_boxes_tensor, iou_threshold=iou_threshold)
         class_boxes = K.gather(class_boxes, nms_index)
