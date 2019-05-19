f04545ad53e9593f6ee00ff88fe36d7801e1a882
Shuai <liustark@hotmail.com>
2019-05-19 21:00:17 +0800

    rm

--- YOLOv3-Keras/yolo.py
+++ YOLOv3-Keras/yolo.py
@@ -21,10 +21,10 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/trained_weights_final.h5',
+        "model_path": 'model_data/yolo-tiny.h5',
         # "model_path": 'model_data/yolo-tiny.h5',
         "anchors_path": 'model_data/tiny_yolo_anchors.txt',
-        "classes_path": 'model_data/voc_classes.txt',
+        "classes_path": 'model_data/coco_classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
